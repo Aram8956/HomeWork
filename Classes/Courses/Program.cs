d8433112a4aaa98c29ce1d18becb1e8b847b03fe
Aram Zakaryan <aram.zakaryan10@gmail.com>
2025-03-20 14:18:58 +0400

    Update Program.cs

--- Classes/Courses/Program.cs
+++ Classes/Courses/Program.cs
@@ -1,4 +1,15 @@
-﻿namespace Class;
+/*Ստեղծել, Course, Group, Module կլասները
+Course - ինֆորմացիա է պարունակում դասընթացի մասին
+(անուն,  ամսավճար, մոդուլներ)
+Module - ինֆորմացիա է պարունակում դասընթացի առանձին կտորի մասին
+(վերնագիր, տևողություն)
+Group - Ներկայացնում է խմբի տվյլաները
+(անուն, ուսանողների քանակ, կուրս)
+Course կլասից ժառանգվում են Web, Game, AI  կլասները
+ընդ որում Web-ը ունի նաև type դաշտ, որը կարող է լինել frontend, backend, fullstack
+Game-ը ունի engine դաշտ, որը կարող է լինել unity, unreal:
+Course,Module և Group, Course կապը has a է, իսկ Course=>Web,Game,Ai -ը is a:
+namespace Class;*/
 
 class Program
 {
